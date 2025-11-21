0ac79eb912a1346a24f2b62a75ed6fd2c0ff90de
Karim El-Refai <115693617+el-refai@users.noreply.github.com>
2025-11-20 19:35:06 -0800

    Fix directory name in env_3dgs_to_mesh.sh

--- env_3dgs_to_mesh.sh
+++ env_3dgs_to_mesh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 conda env create -f sugar_environment.yml
 conda activate r2r2r_sugar
-cd dependencies/SuGaR/gaussian_splatting/submodules/simple_knn
+cd dependencies/SuGaR/gaussian_splatting/submodules/simple-knn
 pip install -e .
 # NVCC_FLAGS="-gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_80,code=sm_80" pip install git+https://github.com/nerfstudio-project/gsplat.git@v1.4.0
 pip install gsplat==1.4.0
